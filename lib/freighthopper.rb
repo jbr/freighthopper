738900ccfe95c484fef7a5c94645c3b18b79377b
Jacob Rothstein <github@jacobrothstein.com>
2009-10-16 00:57:20 -0700

    Kernel#unindent so heredocs aren't ugly

--- lib/freighthopper.rb
+++ lib/freighthopper.rb
@@ -75,6 +75,17 @@ module Kernel
       return *args
     end
   end
+  
+  def unindent(string, options = {})
+    tablength = options[:tablength] || 2
+    lines = string.gsub("\t", " " * tablength).split("\n")
+
+    whitespace = lines.map do |line|
+      line.match(/^(\s+)/).captures.first
+    end.min{ |l, r| l.length <=> r.length }
+
+    lines.map{ |l| l.gsub /^#{whitespace}/, ''}.join("\n")
+  end
 end
 
 # Kernel.trace_output = true
