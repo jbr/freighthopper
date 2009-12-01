b58f1e7abd9c805944943530607eb8b965575877
Jacob Rothstein <github@jacobrothstein.com>
2009-11-30 18:20:39 -0800

    bundle away

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,11 @@
-require File.instance_eval { expand_path join(dirname(__FILE__), '..', 'vendor', 'gems', 'environment')}
+begin
+  require File.instance_eval { expand_path join(dirname(__FILE__), '..', 'vendor', 'gems', 'environment')}
+rescue LoadError
+  puts "Bundling Gems\n\nHang in there, this only has to happen once...\n\n"
+  system 'gem bundle'
+  retry
+end
+
 Bundler.require_env :test
 
 $:.unshift File.instance_eval { expand_path join(dirname(__FILE__), "..", "lib") }
