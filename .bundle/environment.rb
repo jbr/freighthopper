a76fc24ca91ff1d4c586dc5e02f9dfa17dadf175
Jacob Rothstein <github@jacobrothstein.com>
2010-02-19 14:43:26 -0800

    specify activesupport version

--- .bundle/environment.rb
+++ .bundle/environment.rb
@@ -2,8 +2,8 @@ require 'digest/sha1'
 
 # DO NOT MODIFY THIS FILE
 module Bundler
-  FINGERPRINT = "588b0e9a1b6bf1d427a36ed6551864812cf2fd9d"
-  LOAD_PATHS = ["/Library/Ruby/Gems/1.8/gems/activesupport-2.3.5/lib", "/Library/Ruby/Gems/1.8/gems/shoulda-2.10.3/lib", "/Library/Ruby/Gems/1.8/gems/test-rig-0.0.3/lib"]
+  FINGERPRINT = "6e72130f9766a97765b3aeb6320cca2b5bad26cf"
+  LOAD_PATHS = ["/Library/Ruby/Gems/1.8/gems/activesupport-2.3.5/lib", "/Library/Ruby/Gems/1.8/gems/test-rig-0.0.3/lib", "/Library/Ruby/Gems/1.8/gems/shoulda-2.10.3/lib"]
   AUTOREQUIRES = {:test=>["test_rig", "shoulda"], :default=>["active_support"]}
 
   def self.match_fingerprint
